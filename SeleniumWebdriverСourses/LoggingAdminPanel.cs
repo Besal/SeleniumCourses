bfdb19cd5af229dc5d5bec76c127bd14cbf6aaa8
Besal <31038166+Besal@users.noreply.github.com>
2017-08-17 17:12:42 +0300

    Added test class for checking admin panel.

---
+++ SeleniumWebdriverСourses/LoggingAdminPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumWebdriverСourses
+{
+    [TestFixture(BrowserType.Chrome)]
+    //[TestFixture(BrowserType.Ie)]
+    //[TestFixture(BrowserType.Firefox)]
+    public class LoggingAdminPanel : SeleniumWebdriverBase
+    {
+        public LoggingAdminPanel(BrowserType browser) 
+            : base(browser)
+        {
+        }
+
+        [Test]
+        public void CheckSomeElementsInAdminPanel()
+        {
+            var listOfSections = new List<string>
+            {
+                "Appearence",
+                "Catalog",
+                "Countries",
+                "Currencies",
+                "Customers",
+                "Geo Zones"
+            };
+
+            foreach (var section in listOfSections)
+            {
+                Assert.IsFalse(CheckElementExists(
+                    By.XPath($"//*[@id='app-']//span[@class='name' and contains(text(), '{section}')]")));
+            }
+        }
+    }
+    
+}
