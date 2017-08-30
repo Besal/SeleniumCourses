008cd6fbd79a130826fef23b0e5b877f0aae5f7f
Besal <31038166+Besal@users.noreply.github.com>
2017-08-30 13:01:01 +0300

    Added test for all checks sorting

--- SeleniumWebdriverСourses/CheckLabelsInProducts.cs
+++ SeleniumWebdriverСourses/CheckLabelsInProducts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -10,7 +11,7 @@ namespace SeleniumWebdriverСourses
     [TestFixture(BrowserType.Firefox)]
     public class CheckLabelsInProducts : SeleniumWebdriverBase
     {
-        public CheckLabelsInProducts(BrowserType browser) 
+        public CheckLabelsInProducts(BrowserType browser)
             : base(browser)
         {
         }
@@ -18,10 +19,12 @@ namespace SeleniumWebdriverСourses
         [Test]
         public void CheckStickers()
         {
-            FindByXpathAndClick(".//*[@id='sidebar']//i[@class='fa fa-chevron-circle-left']", "кнопку главной страницы");
+            FindByXpathAndClick(".//*[@id='sidebar']//i[@class='fa fa-chevron-circle-left']",
+                "кнопку главной страницы");
             var listOfProducts = Driver
                 .FindElements(By.XPath("//div[@class='image-wrapper']"))
-                .Select(el => el.FindElements(By.XPath("./div[contains(@class, 'sticker') and contains(text(), '')]"))).ToList();
+                .Select(el => el.FindElements(By.XPath("./div[contains(@class, 'sticker') and contains(text(), '')]")))
+                .ToList();
             foreach (var element in listOfProducts)
             {
                 var countOfStickers = element.Count(sticker => sticker.Displayed);
@@ -30,27 +33,66 @@ namespace SeleniumWebdriverСourses
         }
 
         [Test]
-        public void CheckCountriesSorting()
+        public void CheckCountriesBlockSorting()
         {
             FindByXpathAndClick("//span[@class='name' and contains(text(), 'Countries')]", "кнопку стран");
-            var list = Driver.FindElements(By.XPath("//table[@class='dataTable']//tr[@class='row']/td[5]/a[@href and text()]"))
+            var listOfCoutries = GetListOfElementsText("//table[@class='dataTable']//tr[@class='row']/td[5]/a[@href and text()]");
+                CheckPairSorted(listOfCoutries);
+
+            var nonZeroZonesLinks =
+                GetListOfElementsWithAttribute(".//tbody/tr[@class='row' and td[position()=6 and text()!='0']]/td[5]/a",
+                    "href");
+            foreach (var elementLink in nonZeroZonesLinks)
+            {
+                Driver.FindElement(By.XPath($"//a[@href='{elementLink}']")).Click();
+                var zonesInCountry = GetListOfElementsText(".//*[@id='table-zones']//td[text()][3]");
+                    CheckPairSorted(zonesInCountry);
+                Driver.Navigate().Back();
+            }
+        }
+        
+        [Test]
+        public void CheckGeozonesBlockSorting()
+        {
+            FindByXpathAndClick("//span[@class and text()='Geo Zones']", "кнопку гео зон");
+            var geoZonesLinks = GetListOfElementsWithAttribute("//td/a[@href and not(@title)]", "href");
+            foreach (var zonesLink in geoZonesLinks)
+            {
+                Driver.FindElement(By.XPath($"//a[@href='{zonesLink}']")).Click();
+                var listOfZonesForEdit =
+                    GetListOfElementsText(".//*[@id='table-zones']//td[3]//option[@selected='selected' and @value]");
+                    CheckPairSorted(listOfZonesForEdit);
+                Driver.Navigate().Back();
+            }
+        }
+
+        private List<string> GetListOfElementsWithAttribute(string selector, string attribute)
+        {
+            return Driver.FindElements(By.XPath(selector))
+                .Select(el => el.GetAttribute(attribute))
+                .ToList();
+        }
+        //CollectionAssert.AreEqual(list.OrderBy(e => e), list);
+
+        private static void CheckPairSorted(List<string> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+                Assert.AreEqual(1, string.Compare(list[i], list[i - 1], StringComparison.InvariantCulture),
+                "Страны находятся не в алфавитном порядке");
+        }
+
+        private List<string> GetListOfElementsText(string selector)
+        {
+            return Driver.FindElements(By.XPath(selector))
                 .Select(el => el.Text)
                 .ToList();
-
-            for (int i = 1; i < list.Count; i++)
-            {
-                Assert.AreEqual(1, string.Compare(list[i], list[i - 1], StringComparison.InvariantCulture));
-            }
-            //CollectionAssert.AreEqual(list.OrderBy(e => e), list);
-
         }
 
         public override void TearDown()
         {
             Driver.Close();
-            Driver.Quit(); 
+            Driver.Quit();
         }
     }
-
-    
 }
+
