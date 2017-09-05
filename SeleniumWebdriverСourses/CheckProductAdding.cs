939a0e7af33410b06dd3a6cdd463a676eabc5792
Besal <31038166+Besal@users.noreply.github.com>
2017-09-05 19:16:09 +0300

    Added test of checking product adding. Added product image.

---
+++ SeleniumWebdriverСourses/CheckProductAdding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumWebdriverСourses
+{
+    [TestFixture(BrowserType.Chrome)]
+    [TestFixture(BrowserType.Firefox)]
+    //[TestFixture(BrowserType.Ie)]
+    class CheckProductAdding : SeleniumWebdriverBase
+    {
+        public CheckProductAdding(BrowserType browser) 
+            : base(browser)
+        {
+        }
+
+        [Test]
+        public void AddProduct()
+        {
+            string pathToFile = AppDomain.CurrentDomain.BaseDirectory + @"\Gizmo.jpg";
+            FindByXpathAndClick(".//*[@id='app-']/a/span[text()='Catalog']", "кнопку каталога");
+            FindByXpathAndClick(".//*[@id='content']//a[text()=' Add New Product']", "кнопку добавления товара");
+            FindByNameAndType("name[en]", "Gizmo");
+            FindByNameAndType("code", "GZM");
+            FindByNameAndClick("product_groups[]", "кнопку выбора пола");
+            Driver.FindElement(By.XPath(".//*[@id='tab-general']//input[@name='new_images[]']")).SendKeys(pathToFile);
+        }
+    }
+}
